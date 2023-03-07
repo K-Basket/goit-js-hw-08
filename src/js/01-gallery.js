8dbbb73b9a0f3bae4aec2cb944337bd7d45c45cb
K-Basket <koshykkv@gmail.com>
2023-03-07 16:54:24 +0200

    add task 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+
+let galleryLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+    `;
+    })
+    .join('');
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+// импорт библиотеки lodash throttle (https://www.npmjs.com/package/lodash.throttle)
+import lodashThrottle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', lodashThrottle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+restoresFormData();
+
+function onFormInput(evt) {
+  const formData = {};
+  // добавляет свойства (ключ и значение) в объект данных формы
+  formData[evt.target.name] = evt.target.value;
+  // записывает  данные в web-хранилище Local Storage
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  // отмена дефолтных настроек
+  evt.preventDefault();
+  // Очистить форму после отправки
+  evt.currentTarget.reset();
+  // Очистить web-хранилище
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function restoresFormData() {
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (formData) {
+    // деструктуризация св-в объекта с пустыми дефолтными значениями
+    const { email = '', message = '' } = formData;
+
+    input.value = email;
+    textarea.value = message;
+  }
+}
