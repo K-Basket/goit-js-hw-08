995d2cb55cb33884c96ba9dcdb9f2c008a8e530c
K-Basket <koshykkv@gmail.com>
2023-03-07 19:03:30 +0200

    Update 02-video.js

---
+++ src/js/02-video.js
@@ -0,0 +1,18 @@
+import lodashThrottle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
+const timeData = JSON.parse(localStorage.getItem(CURRENT_TIME));
+
+// слушаттель события timeupdate
+player.on('timeupdate', lodashThrottle(onPlay, 1000));
+// восстанавливает время остановки player
+player.setCurrentTime(timeData);
+
+function onPlay(data) {
+  const timeData = JSON.stringify(data.seconds);
+
+  localStorage.setItem(CURRENT_TIME, timeData);
+}
